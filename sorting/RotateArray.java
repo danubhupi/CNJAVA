8d67bb48d8fa043c58b23fb40bb1e85f414fe81d
Bhupendra Singh <danubhupi@gmail.com>
2022-04-13 09:14:22 +0530

    Rotate Arrays 2

--- sorting/RotateArray.java
+++ sorting/RotateArray.java
@@ -3,15 +3,44 @@ package sorting;
 import java.util.Scanner;
 
 public class RotateArray {
+	
+	
+	public static void reverse(int ar[],int index,int start) {
+		
+		
+		int n=index;
+
+	while(start<n) {
+		
+		int temp=ar[start];
+		ar[start]=ar[n];
+		ar[n]=temp;
+		start++;
+		n--;
+	}
+		
+	}
 
 	public static void RA(int [] ar,int r) {
-		for(int i=0;i<r;i++) {
-			int temp=ar[0],j=0;
-			for(j=0;j<ar.length-1;j++) {
-				ar[j]=ar[j+1];
-			}
-			ar[j]=temp;
-		}
+		
+		// brute force
+		
+//		for(int i=0;i<r;i++) {
+//			int temp=ar[0],j=0;
+//			for(j=0;j<ar.length-1;j++) {
+//				ar[j]=ar[j+1];
+//			}
+//			ar[j]=temp;
+//		}
+		
+		//Reverse part of arrays
+		int n=ar.length;
+		
+
+		reverse(ar,ar.length-1,0);
+		reverse(ar,n-r-1,0);
+		reverse(ar,n-1,n-r);
+		
 		
 		print(ar);
 		
@@ -31,7 +60,7 @@ public class RotateArray {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-Scanner sc=new Scanner (System.in);
+      Scanner sc=new Scanner (System.in);
 		
 		int n=sc.nextInt();
 		int [] ar=new int[n];
@@ -41,6 +70,7 @@ Scanner sc=new Scanner (System.in);
 		}
 		int r=sc.nextInt();
 		RA(ar,r);
+		sc.close();
 
 	}
 
