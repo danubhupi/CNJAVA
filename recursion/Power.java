4272adb7fc0d6332cd07eb266379e4d709306507
Bhupendra Singh <danubhupi@gmail.com>
2022-05-05 05:59:28 +0530

    Power Second Method

--- recursion/Power.java
+++ recursion/Power.java
@@ -2,18 +2,27 @@ package recursion;
 
 public class Power {
 	
-	public static int Pow(int n,int exp) {
-		if(exp<0) {return -1;}
-		if(exp==0) {return 1;}
+//	public static int Pow(int n,int exp) {
+//		if(exp<0) {return -1;}
+//		if(exp==0) {return 1;}
+//		
+//		return n*Pow(n,exp-1);
+//		
+//	}
+	
+	public static int Pow(int x,int y) {
+		if(x==0)return 0;
+		if(y==0)return 1;
 		
-		return n*Pow(n,exp-1);
-		
+		int temp=Pow(x,y/2);
+		if(y%2==0)return temp*temp;
+		else return x*temp*temp;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(Pow(4,0));
+		System.out.println(Pow(4,3));
 
 	}
 
