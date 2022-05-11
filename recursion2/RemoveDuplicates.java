ad79aa4ba0505c6e7bedf4cb77b018eaab0cd275
Bhupendra Singh <danubhupi@gmail.com>
2022-05-11 07:19:17 +0530

    Remove Duplicates Better

--- recursion2/RemoveDuplicates.java
+++ recursion2/RemoveDuplicates.java
@@ -16,12 +16,29 @@ public class RemoveDuplicates {
 		return temp + removeDup(s.substring(1),s.charAt(0));
 		
 	}
+	
+	
+	public static String removeDupBetter(String s) {
+		if(s.length()<=1) {
+			return s;
+		}
+		
+		if(s.charAt(0)==s.charAt(1)) {
+			return removeDupBetter(s.substring(1));
+		}
+		else
+			return s.charAt(0)+ removeDupBetter(s.substring(1));
+		
+	}
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
 		String s=sc.nextLine();
-		 System.out.println(removeDup(s,'\n'));
+//		 System.out.println(removeDup(s,'\n'));
+		System.out.println(removeDupBetter(s));
 		 sc.close();
 
 	}
