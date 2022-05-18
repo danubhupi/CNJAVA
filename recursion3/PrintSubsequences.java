e50e9a8cc4a17babd466b8617b48705834150257
Bhupendra Singh <danubhupi@gmail.com>
2022-05-18 11:45:41 +0530

    Print Subsequences

---
+++ recursion3/PrintSubsequences.java
@@ -0,0 +1,32 @@
+package recursion3;
+
+public class PrintSubsequences {
+	
+	public static void printSubSeq(String input, String output) {
+		
+		if(input=="") {
+			System.out.println(output);
+			return;
+		}
+		
+		printSubSeq(input.substring(1),output);
+		printSubSeq(input.substring(1),output+input.charAt(0));
+		return;
+		
+	}
+	
+	public static void printSubSeq(String input) {
+		printSubSeq(input,"");
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		printSubSeq("xyz");
+		
+
+	}
+
+}
