1f5cd576e326eb6ddc86e04df5d23f476a31867d
Bhupendra Singh <danubhupi@gmail.com>
2022-05-24 07:53:24 +0530

    Coding nijas soln

--- dynamicProgramming1/MST1CN.java
+++ dynamicProgramming1/MST1CN.java
@@ -1,35 +1,33 @@
 package dynamicProgramming1;
 
-public class MinStepsTo1IDP {
-
+public class MST1CN {
 	
-	public static int min(int n) {
+public static int min(int n) {
 		
 		int []dp=new int[n+1];
 		dp[0]=Integer.MAX_VALUE;
 		dp[1]=0;
 		for(int i=2;i<n+1;i++) {
+			int sub1=dp[n-1];
+			int div2=Integer.MAX_VALUE;
+			int div3=Integer.MAX_VALUE;
 			if(i%2==0) {
-				dp[i]=1+Math.min(dp[i/2], dp[i-1]);
+				div2=dp[i/2];
 				
 			}
-			else if(i%3==0) {
-				dp[i]=1+Math.min(dp[i/3], dp[i-1]);
+			 if(i%3==0) {
+				div3=dp[i/3];
 			}
-			else {
-				dp[i]=1+ dp[i-1];
-			}
+			dp[i]=1+Math.min(sub1,Math.min(div2, div3));
 			
 			
 		}
 		return dp[n];
 		
 	}
-	
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(min(11));
 
 	}
 
